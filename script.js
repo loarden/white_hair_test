86070880d9a82219b7f59e230376f0a8c9f1edf4
loarden <biacsi.ors@gmail.com>
2023-10-16 20:21:41 +0200

    fixing select placeholder

--- script.js
+++ script.js
@@ -72,8 +72,7 @@ function validateInput() {
 }
 // Email validation
 function isValidEmail(email) {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
   return re.test(String(email).toLowerCase());
 }
 // Set Error message
